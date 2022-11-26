e17a272ac9ecd2a7717fa6f1818dab16f63c9ccf
markfialko <markfialkofialko@gmail.com>
2022-11-26 23:20:27 +0500

    Добавил класс телеграм бота для отправки эвентов с телеграм API

---
+++ src/main/java/entity/HeadHunterBot.java
@@ -0,0 +1,54 @@
+package entity;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Класс телеграм бота
+ */
+public class HeadHunterBot extends TelegramLongPollingBot {
+
+    private final String BOT_NAME = "@HeadHunterURFUBot";
+    private final String BOT_TOKEN = "5818376016:AAEqj-nTZBkY8y3ziImT1RblB-m1T6t4EZg";
+    private final int RECONNECT_PAUSE = 1000;
+
+    public final Queue<Object> sendQueue = new ConcurrentLinkedQueue<>();
+    public final Queue<Object> receiveQueue = new ConcurrentLinkedQueue<>();
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        receiveQueue.add(update);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    public void botConnect() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiException e) {
+            try {
+                Thread.sleep(RECONNECT_PAUSE);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+                return;
+            }
+            botConnect();
+        }
+    }
+}
