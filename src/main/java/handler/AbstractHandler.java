bd282710fede28f50464c158f4d8f45d072daaba
markfialko <markfialkofialko@gmail.com>
2022-11-26 23:18:45 +0500

    Добавил абстрактный класс хэндлера для обработки различных комманд

---
+++ src/main/java/handler/AbstractHandler.java
@@ -0,0 +1,27 @@
+package handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import command.ParsedCommand;
+import entity.HeadHunterBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * Хендлер для обработки комманд
+ */
+public abstract class AbstractHandler {
+    HeadHunterBot bot;
+
+    AbstractHandler(HeadHunterBot bot) {
+        this.bot = bot;
+    }
+
+    /**
+     * Добавляет в очередь новое сообщение
+     * @param chatId чат из телеграма
+     * @param parsedCommand - спарсенная команда
+     * @param update - эвент из телеграмма
+     * @throws JsonProcessingException
+     */
+    public abstract void operate(String chatId, ParsedCommand parsedCommand, Update update) throws JsonProcessingException;
+
+}
