a400b93270adf04940f6bca812628759cdcd5864
markfialko <markfialkofialko@gmail.com>
2022-12-12 01:46:51 +0500

    Изменил поведение хендлера с командой пользователя(Получение данных с api при авторизации)

--- src/main/java/handler/UserHandler.java
+++ src/main/java/handler/UserHandler.java
@@ -1,22 +1,19 @@
 package handler;
 
-import api.API;
 import auth.Auth;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import command.ParsedCommand;
+import database.user.UserBase;
+import database.user.Users;
 import entity.HeadHunterBot;
 import entity.UserHH;
-import entity.Vacancy;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -31,47 +28,38 @@ public class UserHandler extends AbstractHandler {
     public void operate(String chatId, ParsedCommand parsedCommand, Update update) throws IOException, ExecutionException, InterruptedException {
 
         Integer telegramId = Math.toIntExact(update.getMessage().getChat().getId());
+        Users user = new UserBase().findByTelegramId(telegramId);
 
-            Session session = HibernateUtil.getSessionFactory().openSession();
-
-            session.getTransaction().begin();
-
-            Query query = session.createSQLQuery("SELECT * FROM users WHERE telegram_id = :id");
-            query.setParameter("id",telegramId);
-
-            List<Object[]> response  = query.getResultList();
-
-            String accessToken = response.get(0)[1].toString();
-
+        if (user != null) {
+            String accessToken = user.getAccessToken();
             ObjectMapper mapper = new ObjectMapper();
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
 
+            String response = Auth.getResponse(accessToken);
 
-
-            String re = Auth.getResponse(accessToken);
-
-            UserHH newUser =  mapper.readValue(re, UserHH.class);
-
-            String text = newUser.getEmail();
-
-        bot.sendQueue.add(getUserInfo(chatId, update,text));
+            if (!Objects.equals(response, "Ошибка при авторизации")) {
+                UserHH newUser = mapper.readValue(response, UserHH.class);
+                bot.sendQueue.add(getUserInfo(chatId, newUser.toString()));
+            } else {
+                bot.sendQueue.add(new SendMessage(chatId,"/auth"));
+            }
+        } else {
+            bot.sendQueue.add(new SendMessage(chatId,"/auth"));
+        }
 
     }
 
-
-
     /**
      * Получить информацию о пользователе
+     *
      * @param chatId чат телеграм
-     * @param update эвент телеграм
      * @return сообщение для телеграм бота
      */
-    private SendMessage getUserInfo(String chatId, Update update,String text) {
+    private SendMessage getUserInfo(String chatId, String text) {
         SendMessage message = new SendMessage();
 
         message.setChatId(chatId);
-
 
         message.setText(text);
 
